4847db38ba3d40667413ea7cb4ced4894ce4d5bc
soekopaacykel <eaaalov@students.eaaa.dk>
2024-11-28 10:16:40 +0100

    Opsætning

---
+++ CatalogTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace CatalogTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Eventuel opsætning før hver test
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Testlogik
+            Assert.Pass(); // Testen vil passere automatisk
+        }
+    }
+}
