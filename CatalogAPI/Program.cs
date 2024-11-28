89168c977f7eab6c12fada3ac5280e8e4a16a363
soekopaacykel <eaaalov@students.eaaa.dk>
2024-11-28 11:15:17 +0100

    Revert "Merge pull request #10 from Gron-og-Olsen/3-add-logging-med-loki-og-grafana-for-katalog"
    This reverts commit 46c86f563efa2a3c214b417b1296e31d1fa1cba4, reversing
    changes made to c4d87ec2a01da30e5cd825ef0d2d01b692043c92.

--- CatalogAPI/Program.cs
+++ CatalogAPI/Program.cs
@@ -1,44 +1,25 @@
-using NLog;
-using NLog.Web;
+var builder = WebApplication.CreateBuilder(args);
 
-try
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings()
-        .GetCurrentClassLogger();
-    logger.Debug("init main");
-
-    var builder = WebApplication.CreateBuilder(args);
-
-    // Add services to the container.
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-    builder.Logging.ClearProviders();
-    builder.Host.UseNLog();
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-    app.UseAuthorization();
-    app.MapControllers();
-
-    app.Run();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-catch (Exception ex)
-{
-    var logger = NLog.LogManager.GetCurrentClassLogger();
-    logger.Error(ex, "Stopped program because of exception");
-    throw;
-}
-finally
-{
-    NLog.LogManager.Shutdown();
-}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
