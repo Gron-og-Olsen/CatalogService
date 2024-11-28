bbd66c80223a79c2693988f01d72454d0641d723
soekopaacykel <eaaalov@students.eaaa.dk>
2024-11-28 12:15:59 +0100

    Mongoconnection sat op, mangler connectionstring

--- CatalogAPI/Models/Product.cs
+++ CatalogAPI/Models/Product.cs
@@ -1,15 +1,29 @@
-namespace Models;
-public class Product
+namespace Models
 {
-public Guid Id { get; set; }
-public string? Name { get; set; }
-public string? Description { get; set; }
-public decimal Price { get; set; }
-public string? Brand { get; set; }
-public string? Manufacturer { get; set; }
-public string? Model { get; set; }
-public string? ImageUrl { get; set; }
-public string? ProductUrl { get; set; }
-public DateTime ReleaseDate { get; set; }
-public DateTime? ExpiryDate { get; set; }
+    public enum Category
+    {
+        Electronics,
+        Clothing,
+        HomeAppliances,
+        Books,
+        Paintings,
+        
+        // tilføj eventuelt flere kategorier
+    }
+
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public Category Category { get; set; } // Ændret fra string til enum
+        public decimal Price { get; set; }
+        public string? Brand { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Model { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? ProductUrl { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+    }
 }
